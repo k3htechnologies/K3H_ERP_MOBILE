cd0a5c91905f5650fc0b32bf5943056beb0ea639
Rushikesh <rushikesh.hrishabraj007@gmail.com>
2026-02-12 17:52:32 +0530

    code

--- lib/di/feature_dependencies/sales/other_charges/other_charges.dependencies.dart
+++ lib/di/feature_dependencies/sales/other_charges/other_charges.dependencies.dart
@@ -2,7 +2,6 @@ import 'package:get_it/get_it.dart';
 import 'package:k3h_erp_app/features/sales/other_charges/data/datasource/other_charges.datasource.dart';
 import 'package:k3h_erp_app/features/sales/other_charges/data/repository/other_charges.repository.dart';
 import 'package:k3h_erp_app/features/sales/other_charges/presentation/cubit/other_charges_cubit.dart';
-import 'package:k3h_erp_app/features/sales/other_charges/presentation/cubit/other_charges_cubit.dart';
 
 void registerOtherChargesDependencies(GetIt serviceLocator) {
   serviceLocator.registerSingleton<OtherChargesDatasource>(
