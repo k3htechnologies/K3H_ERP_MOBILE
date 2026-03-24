417384fbab12f2d1a5f0a2e90e490614c7cba6d3
shubhangiHRR <shubhangi.hrishabraj@gmail.com>
2026-03-24 11:59:26 +0530

    unused removed

--- lib/features/inventory/presentation/pages/inventory_screen.dart
+++ lib/features/inventory/presentation/pages/inventory_screen.dart
@@ -19,7 +19,6 @@ import 'package:k3h_erp_app/utils/utility_function.dart';
 import 'package:k3h_erp_app/widgets/app_bar/custom_app_bar_with_back_button.dart';
 import 'package:k3h_erp_app/widgets/approve_reject_widget.dart';
 import 'package:k3h_erp_app/widgets/buttons/custom_button.dart';
-import 'package:k3h_erp_app/widgets/buttons/custom_icon_button.dart';
 import 'package:k3h_erp_app/widgets/chip_style_tab_bar.dart';
 import 'package:k3h_erp_app/widgets/custom_common_widget.dart';
 import 'package:k3h_erp_app/widgets/utils_widgets.dart';
