d30f5a43bdafb870509a6f9f35ad04034d6df43e
rushikesh490 <rushikeshavate490@gmail.com>
2026-03-26 14:28:55 +0530

    code

--- lib/features/login/presentation/pages/splash_screen.dart
+++ lib/features/login/presentation/pages/splash_screen.dart
@@ -82,7 +82,6 @@ class _SplashMobileScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    print("splash/Home screen build");
     return Scaffold(body: Center(child: Image.asset(AppAssets.splashLogoGif)));
   }
 }
