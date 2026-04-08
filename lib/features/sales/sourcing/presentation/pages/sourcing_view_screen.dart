8825118c79b9c9b842fbf263741f7a3f13b6ce63
rushikesh490 <rushikeshavate490@gmail.com>
2026-04-08 15:03:09 +0530

    code

--- lib/features/sales/sourcing/presentation/pages/sourcing_view_screen.dart
+++ lib/features/sales/sourcing/presentation/pages/sourcing_view_screen.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:k3h_erp_app/core/models/project.model.dart';
 import 'package:k3h_erp_app/core/route_authorization.dart';
 import 'package:k3h_erp_app/features/channel_partner/data/model/channel_partner.model.dart';
 import 'package:k3h_erp_app/features/sales/sourcing/data/model/sourcing.model.dart';
@@ -12,7 +11,6 @@ import 'package:k3h_erp_app/style/app_color.dart';
 import 'package:k3h_erp_app/style/text_style.dart';
 import 'package:k3h_erp_app/utils/common_function.dart';
 import 'package:k3h_erp_app/utils/dialog_helper.dart';
-import 'package:k3h_erp_app/utils/utility_function.dart';
 import 'package:k3h_erp_app/widgets/app_bar/custom_app_bar_with_back_button.dart';
 import 'package:k3h_erp_app/widgets/buttons/custom_button.dart';
 import 'package:k3h_erp_app/widgets/buttons/custom_icon_button.dart';
@@ -55,9 +53,6 @@ class _SourcingViewScreenState extends State<SourcingViewScreen>
 
   late Map<String, dynamic> selectedSupport;
 
-  // PROJECT
-  late ProjectModel _project;
-
   // STATIC SUPPORT LIST
   List<Map<String, dynamic>> supportList = [
     {"zAttributesId": -1, "DisplayName": "Select Support"},
@@ -74,7 +69,6 @@ class _SourcingViewScreenState extends State<SourcingViewScreen>
 
     _routeAuthorizationModel =
         Authorization.routeAuthorizationMap[AppRoutes.sourcing]!;
-    _project = getProject();
     _remarkC = TextEditingController();
     selectedSupport = supportList.first;
     _tabController = TabController(length: 2, vsync: this);
