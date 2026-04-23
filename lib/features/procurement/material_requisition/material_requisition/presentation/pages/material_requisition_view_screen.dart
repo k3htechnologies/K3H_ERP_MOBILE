86c05116b42c36add502ad49bfbd515e05a1f081
shubhangiHRR <shubhangi.hrishabraj@gmail.com>
2026-04-23 12:11:53 +0530

    material requistion

--- lib/features/procurement/material_requisition/material_requisition/presentation/pages/material_requisition_view_screen.dart
+++ lib/features/procurement/material_requisition/material_requisition/presentation/pages/material_requisition_view_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:k3h_erp_app/core/route_authorization.dart';
 import 'package:k3h_erp_app/features/procurement/material_requisition/finalize_vendors/data/model/finalize_vendor.model.dart';
 import 'package:k3h_erp_app/features/procurement/material_requisition/finalize_vendors/data/model/finalize_vendor_for_compare.model.dart';
