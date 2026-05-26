e34c44a0cdb1235e16d6aa013cef446769b79d10
shubhangiHRR <shubhangi.hrishabraj@gmail.com>
2026-05-26 17:15:21 +0530

    CP Universe popup fix

--- lib/features/channel_partner/presentation/pages/cp_universe.dart
+++ lib/features/channel_partner/presentation/pages/cp_universe.dart
@@ -4,7 +4,6 @@ import 'package:k3h_erp_app/routes/app_routes.dart';
 import 'package:k3h_erp_app/routes/route_delegate.dart';
 import 'package:k3h_erp_app/style/app_color.dart';
 import 'package:k3h_erp_app/style/text_style.dart';
-import 'package:k3h_erp_app/utils/dialog_helper.dart';
 import 'package:k3h_erp_app/widgets/app_bar/custom_app_bar.dart';
 import 'package:k3h_erp_app/widgets/buttons/custom_icon_button.dart';
 
@@ -16,42 +15,98 @@ class CpUniverseScreen extends StatefulWidget {
 }
 
 class _CpUniverseScreenState extends State<CpUniverseScreen> {
+  void _closeDialog() {
+    Navigator.of(context, rootNavigator: true).pop();
+    goRouter.replaceNamed(AppRoutes.dashboardScreen);
+  }
+
   void _showComingSoonDialog(BuildContext context) {
-    DialogHelper.showCustomDialogue(
-      context,
-      icon: CustomIconButton(
-        onPressed: () {},
-        icon: Icon(
-          Icons.warning_amber_outlined,
-          color: AppColor.yellow,
-          size: 16,
-        ),
-        backgroundColor: AppColor.yellow.withValues(alpha: .2),
-      ),
-      title: "COMING SOON",
-      onDismiss: () => goRouter.replaceNamed(AppRoutes.dashboardScreen),
-      childContent: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Divider(
-            color: AppColor.black.withValues(alpha: 0.50),
-            thickness: 0.5,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-            child: Text(
-              "This feature is currently under development and will be available soon.",
-              style: AppTextStyle.ts14SB(),
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (dialogContext) {
+        return PopScope(
+          canPop: true,
+          onPopInvokedWithResult: (didPop, result) {
+            if (didPop) {
+              goRouter.replaceNamed(AppRoutes.dashboardScreen);
+            }
+          },
+          child: Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Row(
+                      children: [
+                        CustomIconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.warning_amber_outlined,
+                            color: AppColor.yellow,
+                            size: 16,
+                          ),
+                          backgroundColor: AppColor.yellow.withValues(
+                            alpha: .2,
+                          ),
+                        ),
+
+                        const SizedBox(width: 8),
+
+                        Expanded(
+                          child: Text(
+                            "COMING SOON",
+                            style: AppTextStyle.ts16SB(),
+                          ),
+                        ),
+
+                        InkWell(
+                          onTap: _closeDialog,
+                          child: Icon(
+                            Icons.close,
+                            color: AppColor.black,
+                            size: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  Divider(
+                    color: AppColor.black.withValues(alpha: 0.50),
+                    thickness: 0.5,
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
+                      horizontal: 16,
+                    ),
+                    child: Text(
+                      "This feature is currently under development and will be available soon.",
+                      style: AppTextStyle.ts14SB(),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 
   @override
   void initState() {
     super.initState();
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _showComingSoonDialog(context);
     });
@@ -59,12 +114,20 @@ class _CpUniverseScreenState extends State<CpUniverseScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        screenTitle: "CP Universe",
-        authorization: AuthorizationModel(),
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) return;
+
+        goRouter.replaceNamed(AppRoutes.dashboardScreen);
+      },
+      child: Scaffold(
+        appBar: CustomAppBar(
+          screenTitle: "CP Universe",
+          authorization: AuthorizationModel(),
+        ),
+        body: Container(),
       ),
-      body: Container(),
     );
   }
 }
