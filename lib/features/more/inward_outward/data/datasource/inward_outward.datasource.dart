0dc9e3bd7e087be84bea01f1651f0150428e870a
shubhangiHRR <shubhangi.hrishabraj@gmail.com>
2026-06-15 10:17:58 +0530

    INWARD-OUTWARD: Datasource and Repository code done

--- lib/features/more/inward_outward/data/datasource/inward_outward.datasource.dart
+++ lib/features/more/inward_outward/data/datasource/inward_outward.datasource.dart
@@ -1,3 +1,197 @@
-interface class InwardOutwardDatasource {
-  
+import 'package:k3h_erp_app/features/more/inward_outward/data/model/inward_outward.model.dart';
+import 'package:k3h_erp_app/service/base_client.dart';
+import 'package:k3h_erp_app/service/exceptions.dart';
+import 'package:k3h_erp_app/utils/common_function.dart';
+
+abstract interface class InwardOutwardDatasource {
+  Future<Map<String, dynamic>> apicallPullInwardOutwardMaster({
+    required int pageNumber,
+    required int pageSize,
+    Map<String, dynamic>? queryParams,
+  });
+  Future<Map<String, dynamic>> apicallAddUpdateInwardOutwardMaster({
+    required Map<String, String> body,
+    required List<Map<String, dynamic>> fileList,
+  });
+  Future<Map<String, dynamic>> apicallAddUpdateInwardOutwardRevert({
+    required Map<String, String> body,
+    required List<Map<String, dynamic>> fileList,
+  });
+  Future<Map<String, dynamic>> apicallDeleteInwardOutward({
+    required int inwardOutwardId,
+    required String uniqueKey,
+  });
+  Future<Map<String, dynamic>> apicallPullSenderReceiverByMobileNo({
+    required String mobileNumber,
+  });
+}
+
+class InwardOutwardDatasourceImp implements InwardOutwardDatasource {
+  final BaseClient baseClient = BaseClient();
+  @override
+  Future<Map<String, dynamic>> apicallPullInwardOutwardMaster({
+    required int pageNumber,
+    required int pageSize,
+    Map<String, dynamic>? queryParams,
+  }) async {
+    String pullInwardOutwardUrl({
+      required int pageSize,
+      required int pageNumber,
+      Map<String, dynamic>? queryParams,
+    }) {
+      String url =
+          "InwardOutward/PullInwardOutward?PageSize=$pageSize&PageNumber=$pageNumber";
+      url += queryParamsFormatter(queryParams: queryParams);
+      return url;
+    }
+
+    try {
+      var networkResponse = await baseClient.getRequestWithAuthentication(
+        pullInwardOutwardUrl(
+          pageSize: pageSize,
+          pageNumber: pageNumber,
+          queryParams: queryParams,
+        ),
+      );
+      return {
+        'data': List<InwardOutwardModel>.from(
+          networkResponse["data"].map((e) => InwardOutwardModel.fromJson(e)),
+        ),
+        'totalNumberOfRecord': networkResponse['totalNumberOfRecord'],
+      };
+    } catch (error) {
+      if (error is TokenExpiredException) {
+        apicallPullInwardOutwardMaster(
+          pageNumber: pageNumber,
+          pageSize: pageSize,
+          queryParams: queryParams,
+        );
+      }
+      rethrow;
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> apicallAddUpdateInwardOutwardMaster({
+    required Map<String, String> body,
+    required List<Map<String, dynamic>> fileList,
+  }) async {
+    String addUpdateInwardOutwardUrl = "InwardOutward/AddUpdateInwardOutward";
+
+    try {
+      var networkResponse = await baseClient
+          .multipartRequestWithAuthenticationBytes(
+            addUpdateInwardOutwardUrl,
+            fileList,
+            body,
+          );
+      return {
+        'data': List<InwardOutwardModel>.from(
+          networkResponse["data"].map((e) => InwardOutwardModel.fromJson(e)),
+        ),
+        'totalNumberOfRecord': networkResponse['totalNumberOfRecord'],
+      };
+    } catch (error) {
+      if (error is TokenExpiredException) {
+        apicallAddUpdateInwardOutwardMaster(body: body, fileList: fileList);
+      }
+      rethrow;
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> apicallAddUpdateInwardOutwardRevert({
+    required Map<String, String> body,
+    required List<Map<String, dynamic>> fileList,
+  }) async {
+    String addUpdateInwardOutwardRevert =
+        "InwardOutward/AddUpdateInwardOutward";
+
+    try {
+      var networkResponse = await baseClient
+          .multipartRequestWithAuthenticationBytes(
+            addUpdateInwardOutwardRevert,
+            fileList,
+            body,
+          );
+      return {
+        'data': List<InwardOutwardModel>.from(
+          networkResponse["data"].map((e) => InwardOutwardModel.fromJson(e)),
+        ),
+        'totalNumberOfRecord': networkResponse['totalNumberOfRecord'],
+      };
+    } catch (error) {
+      if (error is TokenExpiredException) {
+        apicallAddUpdateInwardOutwardRevert(body: body, fileList: fileList);
+      }
+      rethrow;
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> apicallDeleteInwardOutward({
+    required int inwardOutwardId,
+    required String uniqueKey,
+  }) async {
+    String deleteInwardOutwardUrl({
+      required int inwardOutwardId,
+      required String uniqueKey,
+    }) {
+      return "InwardOutward/DeleteInwardOutward?"
+          "Uniquekey=$uniqueKey"
+          "&InwardOutwardId=$inwardOutwardId";
+    }
+
+    try {
+      var networkResponse = await baseClient.deleteRequestWithAuthentication(
+        deleteInwardOutwardUrl(
+          uniqueKey: uniqueKey,
+          inwardOutwardId: inwardOutwardId,
+        ),
+      );
+
+      return {
+        'data': networkResponse["data"],
+        'totalNumberOfRecord': networkResponse['totalNumberOfRecord'],
+        'message': networkResponse["message"],
+      };
+    } catch (error) {
+      // Handle token expiration by retrying once
+      if (error is TokenExpiredException) {
+        return apicallDeleteInwardOutward(
+          uniqueKey: uniqueKey,
+          inwardOutwardId: inwardOutwardId,
+        );
+      }
+      rethrow;
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> apicallPullSenderReceiverByMobileNo({
+    required String mobileNumber,
+  }) async {
+    String pullSenderReceiverByMobileNo({required String mobileNumber}) {
+      String url =
+          "InwardOutward/PullSenderReceiverByMobileNo?MobileNumber=$mobileNumber";
+      return url;
+    }
+
+    try {
+      var networkResponse = await baseClient.getRequestWithAuthentication(
+        pullSenderReceiverByMobileNo(mobileNumber: mobileNumber),
+      );
+      return {
+        'data': List<InwardOutwardModel>.from(
+          networkResponse["data"].map((e) => InwardOutwardModel.fromJson(e)),
+        ),
+        'totalNumberOfRecord': networkResponse['totalNumberOfRecord'],
+      };
+    } catch (error) {
+      if (error is TokenExpiredException) {
+        apicallPullSenderReceiverByMobileNo(mobileNumber: mobileNumber);
+      }
+      rethrow;
+    }
+  }
 }
--- lib/features/more/inward_outward/data/repository/inward_outward.repository.dart
+++ lib/features/more/inward_outward/data/repository/inward_outward.repository.dart
@@ -1,1 +1,117 @@
-interface class InwardOutwardRepository {}
+import 'package:fpdart/fpdart.dart';
+import 'package:k3h_erp_app/core/error_handler.dart';
+import 'package:k3h_erp_app/core/failure.dart';
+import 'package:k3h_erp_app/features/more/inward_outward/data/datasource/inward_outward.datasource.dart';
+
+abstract interface class InwardOutwardRepository {
+  Future<Either<Failure, Map<String, dynamic>>> getInwardOutwardList({
+    required int pageNumber,
+    required int pageSize,
+    Map<String, dynamic>? queryParams,
+  });
+
+  Future<Either<Failure, Map<String, dynamic>>> addUpdateInwardOutward({
+    required Map<String, String> body,
+    required List<Map<String, dynamic>> fileList,
+  });
+
+  Future<Either<Failure, Map<String, dynamic>>> addUpdateInwardOutwardRevert({
+    required Map<String, String> body,
+    required List<Map<String, dynamic>> fileList,
+  });
+
+  Future<Either<Failure, Map<String, dynamic>>> deleteInwardOutward({
+    required int inwardOutwardId,
+    required String uniqueKey,
+  });
+
+  Future<Either<Failure, Map<String, dynamic>>> getSenderReceiverByMobileNo({
+    required String mobileNumber,
+  });
+}
+
+class InwardOutwardRepositoryImpl implements InwardOutwardRepository {
+  final InwardOutwardDatasource inwardOutwardDatasource;
+
+  InwardOutwardRepositoryImpl({required this.inwardOutwardDatasource});
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> getInwardOutwardList({
+    required int pageNumber,
+    required int pageSize,
+    Map<String, dynamic>? queryParams,
+  }) async {
+    try {
+      var result = await inwardOutwardDatasource.apicallPullInwardOutwardMaster(
+        pageNumber: pageNumber,
+        pageSize: pageSize,
+        queryParams: queryParams,
+      );
+
+      return right(result);
+    } catch (error) {
+      return left(Failure(message: ErrorHandler.getErrorMessage(error)));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> addUpdateInwardOutward({
+    required Map<String, String> body,
+    required List<Map<String, dynamic>> fileList,
+  }) async {
+    try {
+      var result = await inwardOutwardDatasource
+          .apicallAddUpdateInwardOutwardMaster(body: body, fileList: fileList);
+
+      return right(result);
+    } catch (error) {
+      return left(Failure(message: ErrorHandler.getErrorMessage(error)));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> addUpdateInwardOutwardRevert({
+    required Map<String, String> body,
+    required List<Map<String, dynamic>> fileList,
+  }) async {
+    try {
+      var result = await inwardOutwardDatasource
+          .apicallAddUpdateInwardOutwardRevert(body: body, fileList: fileList);
+
+      return right(result);
+    } catch (error) {
+      return left(Failure(message: ErrorHandler.getErrorMessage(error)));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> deleteInwardOutward({
+    required int inwardOutwardId,
+    required String uniqueKey,
+  }) async {
+    try {
+      var result = await inwardOutwardDatasource.apicallDeleteInwardOutward(
+        inwardOutwardId: inwardOutwardId,
+        uniqueKey: uniqueKey,
+      );
+
+      return right(result);
+    } catch (error) {
+      return left(Failure(message: ErrorHandler.getErrorMessage(error)));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> getSenderReceiverByMobileNo({
+    required String mobileNumber,
+  }) async {
+    try {
+      var result = await inwardOutwardDatasource
+          .apicallPullSenderReceiverByMobileNo(mobileNumber: mobileNumber);
+
+      return right(result);
+    } catch (error) {
+      return left(Failure(message: ErrorHandler.getErrorMessage(error)));
+    }
+  }
+}
